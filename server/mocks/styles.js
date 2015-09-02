d55d86e3444ff06f56d8be31eb4917a178835def
Brian Genisio <BrianGenisio@Gmail.com>
2015-09-02 12:11:35 -0400

    make sure that styles are unique

--- server/mocks/styles.js
+++ server/mocks/styles.js
@@ -1,4 +1,5 @@
 module.exports = function(app) {
+  var _ = require('lodash');
   var express = require('express');
   var stylesRouter = express.Router();
   var data = require('../data');
@@ -6,9 +7,15 @@ module.exports = function(app) {
   var idCount = 0;  
 
   var styles = data.bikes.map(function(bike) {
+    return bike.style;
+  });
+
+  styles = _.uniq(styles);
+
+  styles = styles.map(function(style) {
     return {
       id: idCount++,
-      name: bike.style
+      name: style
     };
   });
 
