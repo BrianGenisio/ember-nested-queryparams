c7d6bc4a3e856c08cc5a18eec3db8d8ab88b7b4d
Brian Genisio <BrianGenisio@Gmail.com>
2015-09-02 12:35:07 -0400

    adding checkbox filters

---
+++ app/controllers/bikes.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  proxiedStyles: Ember.computed.map('model.styles', function(model) {
+    return Ember.ObjectProxy.create({
+      content: model,
+      checked: false
+    });
+  }),
+
+  proxiedCheckedStyles: Ember.computed.filterBy('proxiedStyles', 'checked', true),
+  checkedStyles: Ember.computed.mapBy('proxiedCheckedStyles', 'content'),
+
+  stylesObserver: function() {
+    console.log('Checked changed: ', this.get('checkedStyles'));
+  }.observes('checkedStyles.[]').on('init'),
+
+  actions: {
+    checked: function() {
+      console.log('Checked!!!', this.get('checkedStyles'));
+    }
+  }
+});
